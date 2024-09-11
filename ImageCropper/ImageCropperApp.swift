fc37a66f19d905fc3cedfc9f77ae0da73ff8711b
Shirayo <iphoje@gmail.com>
2024-09-11 10:10:20 +0300

    Initial Commit

---
+++ ImageCropper/ImageCropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCropperApp.swift
+//  ImageCropper
+//
+//  Created by Apple on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageCropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
